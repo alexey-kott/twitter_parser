3fd94bf2611a3b28f2ed0b6d652b4164c9932c5b
Alexey Kott <alexey.kott@gmail.com>
2018-04-15 19:26:05 +0300

    Избавился от Selenium, переписал на aiohttp

--- app/async_consumers.py
+++ app/async_consumers.py
@@ -1,144 +1,101 @@
 # -*- coding: utf-8 -*-  
-from channels.generic.websocket import AsyncWebsocketConsumer
-from asyncio import sleep as async_sleep
+import re
 import json
+from asyncio import sleep as async_sleep
 
-import asyncio
-
-from time import sleep
-
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import uuid
-
+from channels.generic.websocket import AsyncWebsocketConsumer
+from aiohttp import ClientSession
 from bs4 import BeautifulSoup
 
-def is_retweet(tweet_block):
-    tweet = tweet_block.find_all(class_="tweet")[0]
-    try:
-        retweeter = tweet['data-retweeter']
+
+def is_retweet(tweet):
+    retweet_text = tweet.find_all(class_="js-retweet-text")
+    if len(retweet_text) > 0:
         return True
-    except:
-        return False
-
-def diff(new_list, old_list):
-    # print(len(new_list))
-    # print(len(old_list))
-    return new_list[len(old_list):]
+    return False
 
 
-driver_tasks = {}
+def parse_tweet_info(tweet):
+    text = tweet.find_all(class_="tweet-text")[0].text
+    text = re.sub(r'(https://[^\s]+)', r' <a target="_blank" href="\1">\1</a> ', text)
+    text = re.sub(r'(pic.twitter.com[^\s]+)', r' <a taget="_blank" href="https://\1">\1</a>', text)
+    text = re.sub(r'@([^\s]+)', r' <a target="_blank" href="https://twitter.com/\1">@\1</a> ', text)
+    timestamp = tweet.find_all(class_="tweet-timestamp")[0]['title']
 
-async def parser(username=None, app_consumer=None, task_uid=None):
-    if username is None:
-        return False
+    return {
+        'text': text,
+        'timestamp': timestamp,
+        'is_retweet': is_retweet(tweet),
+        'tweet_id': tweet['data-tweet-id'],
+        'link': tweet['data-permalink-path']
+    }
 
-    print(f"Start parsing: {username}")
-    options = webdriver.ChromeOptions()
-    # options.add_argument("--start-maximized")
-    options.add_argument("--headless")
-    driver = webdriver.Chrome("./app/webdriver/chromedriver", chrome_options=options)
-    
-    # driver_tasks[task_uid] = driver
-    try:
 
-        driver.get(f"https://www.twitter.com/{username}")
-        driver.find_element_by_tag_name('body').send_keys(Keys.ESCAPE)
+def user_exist(page):
+    if page.find("that page doesn’t exist") == -1:
+        return True
+    return False
 
-        tweet_list = []
-        old_tweet_list = []
 
-        for i in range(1000):
-            driver.find_element_by_tag_name('body').send_keys(Keys.END)
+async def parser(username=None, app_consumer=None):
+    async with ClientSession() as session:
+        async with session.get(f"https://www.twitter.com/{username}") as response:
+            response_data = await response.text()
 
-            source_code = driver.find_element_by_tag_name('body').get_attribute('innerHTML')
-            soup = BeautifulSoup(source_code, "lxml")
-            tweets_block = soup.find(id="stream-items-id")
+            if not user_exist(response_data):
+                await app_consumer.send(text_data=json.dumps({'action': 'user_not_exist'}))
+                return
 
+        soup = BeautifulSoup(response_data, "lxml")
+        stream_container = soup.find_all(class_="stream-container")[0]
+        data_min_position = stream_container['data-min-position']
+
+        tweets = soup.find_all(class_="tweet")
+        tweets = [parse_tweet_info(tweet) for tweet in tweets]
+        await app_consumer.send(text_data=json.dumps(tweets))
             
+        while True:
+            params = {
+                    'include_available_features': '1',
+                    'include_entities': '1',
+                    'max_position': data_min_position,
+                    'reset_error_state': 'false'
+            }
+
+            async with session.get(f"https://twitter.com/i/profiles/show/{username}/timeline/tweets", params=params) as response:
+                response_data = await response.text()
+
+            response_json = json.loads(response_data)
+            data_min_position = response_json['min_position']
+            content = response_json['items_html']
+            if data_min_position is None:
+                await app_consumer.send(text_data=json.dumps({'action': 'complete'}))
+                return
+
+            soup = BeautifulSoup(content, "lxml")
+            tweets = soup.find_all(class_="tweet")
+            tweets = [parse_tweet_info(tweet) for tweet in tweets]
+            await app_consumer.send(text_data=json.dumps(tweets))
             
-            for tweet_block in tweets_block.find_all(class_="stream-item"):
-                
-
-                tweet_text = tweet_block.find_all(class_="tweet-text")[0].text
-                tweet_date = tweet_block.find_all(class_="tweet-timestamp")[0]['title']
-                tweet_id = tweet_block['data-item-id']
-                
-                tweet_list.append({
-                    "text": tweet_text,
-                    "date": tweet_date,
-                    "is_retweet": is_retweet(tweet_block),
-                    "link": f"https://twitter.com/vkozulya/status/{tweet_id}"            
-                    })
-
-            # print(len(tweet_list))
-
-            tweet_diff = diff(tweet_list, old_tweet_list)
-            # if tweet_list == old_tweet_list:
-            #     # self.close()
-            #     print("CONNECTION CLOSE")
-            #     return
 
 
-            old_tweet_list = tweet_list
-            tweet_list = []
-
-            if app_consumer:
-                await async_sleep(0) # иначе сообщения в следующей строке не отправляются
-                await app_consumer.send(text_data=json.dumps(tweet_diff))
-
-                # print('\n')
-                    # print("  ________________________________________________________________________________________________________________________________________________")
-
-    except Exception as e:  
-        print("PARSING END")
-        print(str(e))
-        print(e)
-        driver.close()
 
 
 class AppConsumer(AsyncWebsocketConsumer):
     groups = ["broadcast"]
 
     async def connect(self):
-        # Called on connection.
-        # To accept the connection call:
         await self.accept()
-        # Or accept the connection and specify a chosen subprotocol.
-        # A list of subprotocols specified by the connecting client
-        # will be available in self.scope['subprotocols']
-        # await self.accept("subprotocol")
-        # To reject the connection, call:
-        # await self.close()
+
 
     async def receive(self, text_data=None, bytes_data=None):
-        # Called with either text_data or bytes_data for each frame
-        # You can call:
-        
-        uid = uuid.uuid4()
-        # print(text_data)
         data = json.loads(text_data)
         username = data['username']
+        username = username.lstrip('@')
 
-        await parser(username=username, app_consumer=self, task_uid = uid)
-        # await self.send(text_data=json.dumps({'text': 'lol'}))
+        await parser(username=username, app_consumer=self)
 
-        # for i in range(100000):
-        #     await self.send(text_data=json.dumps({'text': "Hello, world!"}))
-        #     await async_sleep(1)
-
-
-
-        # Or, to send a binary frame:
-        # await self.send(bytes_data="Hello world!")
-        # Want to force-close the connection? Call:
-        # await self.close()
-        # Or add a custom WebSocket error code!
-        # await self.close(code=4123)
 
     async def disconnect(self, close_code):
-        # Called when the socket closes
-        print("DISCONNECT")
-        print(self.__dict__)
         self.close()
         
