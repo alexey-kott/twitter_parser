91ef2add9023273eadb6a3c4fffd60bbcb7c6cfd
Alexey Kott <alexey.kott@gmail.com>
2018-05-03 02:44:49 +0500

    Disable debug mode

--- twitter_parser/settings.py
+++ twitter_parser/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'dlxg)s+bdlnokoxk89yivl+du%&9)s7&1+o7!k&e$@(e%+wlot'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '*'
